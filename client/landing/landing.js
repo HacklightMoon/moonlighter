54ee98b5de3b6c95802dbef5012a027e2c674ce2
Richard Boothe <richardjboothe@me.com>
2016-04-20 14:44:34 -0500

    update favicon

---
+++ client/landing/landing.js
@@ -0,0 +1,7 @@
+angular.module('moonlighter.landing', [])
+.controller('landingCtrl', function(){
+  var landing=this;
+  landing.conjureQuests = function(){
+    //onclick of conjure quests button, view quests
+  }
+});
