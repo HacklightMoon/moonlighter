658db83ebff8bcccccb04d9ad23efee462d79ba2
Richard Boothe <richardjboothe@me.com>
2016-04-21 14:35:36 -0500

    add organization

--- client/landing/landing.js
+++
@@ -1,7 +0,0 @@
-angular.module('moonlighterApp.landing', [])
-.controller('landingCtrl', function(){
-  var landing=this;
-  landing.conjureQuests = function(){
-    //onclick of conjure quests button, view quests
-  }
-});
