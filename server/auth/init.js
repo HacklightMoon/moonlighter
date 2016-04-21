1d6fe155eb1139e10f005fdb5fcb88390d98a22e
John Jacob <jacobjohnrobert@gmail.com>
2016-04-21 14:30:14 -0500

    implement basic github passport functionality.

---
+++ server/auth/init.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let passport = require('passport');
+let User = require('../models/users');
+
+module.exports = function() {
+  passport.serializeUser(function(user, done) {
+    console.log('auth/config.js 8 serializeUser = ', user);
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(user, done) {
+    console.log('deserializeUser == ', user);
+
+    User.verifyId(user.pasid).then(function(data) {
+      console.log('verifyId err = ', data);
+      console.log('user is = ', user);
+      done(null, user);
+    })
+    .catch(function(err){
+      console.log('deserial err = ', err);
+      done(null, user);
+    });
+  });
+};
---
+++ server/models/users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let Promise = require('bluebird'); //remove this once babel is available.
+let db = require('../db');
+let Users = module.exports;
+
+Users.verifyInsert = function(obj){
+  let session = {};
+  session.passid = obj.id;
+
+  //obj.provider === 'github'
+  session.profile_picture = obj._json.avator_url;
+  session.user = obj._json.name || obj.username;
+
+  return db('users').where({
+    passid: session.passid
+  }).then(function(data){
+    if (data.length === 0){
+      return db('users').insert({
+        user: session.user,
+        passid: session.passid,
+        profile_picture: session.profile_picture
+      }).limit(1).then(function(array){
+        console.log('returning sessions (models/users.js, 24)');
+        return session;
+      });
+    } else {
+      console.log('models/users.js, 28 datas = ', data)
+      if (Array.isArray(data)){
+        return data[0];
+      } else {
+        return data;
+      }
+    }
+  })
+}
