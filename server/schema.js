ec37d887fd0bee5e30d626b2bfd14a1d55e7a030
Matthew J. Kelly <matjkel@icloud.com>
2016-04-21 16:58:18 -0500

    Fix schema and tweak gulpfile

--- server/schema.js
+++ server/schema.js
@@ -12,27 +12,33 @@ knex.schema.createTableIfNotExists('users', function(table){
   //achievements
   //...
   //...
-}).schema.createTableIfNotExists('characters', function(table){
+})
+.createTableIfNotExists('characters', function(table){
   table.increments('id').primary();
   table.integer('user_id').references('id').inTable('users');
   table.string('name');
-  //stats
-  //class
-  //...
-  //assets
-  //...
-  //...
-}).schema.createTableIfNotExists('technicalSkills', function(table){
+    //stats
+    //class
+    //...
+    //assets
+    //...
+    //...
+})
+.createTableIfNotExists('technicalSkills', function(table){
   table.increments('id').primary();
   table.string('name');
-}).schema.createTableIfNotExists('usersTechnicalSkills', function(table){
+})
+.createTableIfNotExists('usersTechnicalSkills', function(table){
   table.increments('id').primary();
   table.integer('user_id').references('id').inTable('users');
   table.integer('technical_skill_id').references('id').inTable('technicalSkills');
   table.integer('skill_score'); //like an endorsement
-}.knex.schema.createTableIfNotExists('charactersSkills', function(table){ //fantasy skills atched to characters
-}).then(function(res){
+})
+.createTableIfNotExists('charactersSkills', function(table){ //fantasy skills atched to characters
+})
+.then(function(res){
   console.log('Success Applying Schema');
-}).catch(function(err){
+})
+.catch(function(err){
   console.log('Blah Blah Blah ERROR');
 });
