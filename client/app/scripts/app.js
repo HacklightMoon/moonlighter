67b6f0915547ec5d02355dab61c90e884ce22c36
Richard Boothe <richardjboothe@me.com>
2016-04-22 15:39:45 -0500

    Add structure

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,2 +1,45 @@
 'use strict';
-angular.module('moonlighterApp', []);
+angular.module('moonlighterApp', ['ui.router',
+                                  'ngAnimate',
+                                  'ngMap'
+                                  ])
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/questFeed');
+
+  $stateProvider
+    .state('landing', {
+      url: '/landing',
+      templateUrl: 'partials/landing.html',
+      controller: 'LandingCtrl'
+    })
+    .state('signIn', {
+      url: '/signIn',
+      templateUrl: 'partials/signIn.html',
+      controller: 'SignInCtrl'
+    })
+    .state('questFeed', {
+      url: '/questFeed',
+      templateUrl: 'partials/questFeed.html',
+      controller: 'QuestFeedCtrl'
+    })
+    .state('questProfile', {
+      url: '/questProfile',
+      templateUrl: 'partials/questProfile.html',
+      controller: 'QuestProfileCtrl'
+    })
+    .state('userProfile', {
+      url: '/userProfile',
+      templateUrl: 'partials/userProfile.html',
+      controller: 'UserProfileCtrl'
+    })
+    .state('editProfile', {
+      url: '/editProfile',
+      templateUrl: 'partials/editProfile.html',
+      controller: 'EditProfileCtrl'
+    })
+    .state('editQuest', {
+      url: '/editQuest',
+      templateUrl: 'partials/editQuest.html',
+      controller: 'EditQuestCtrl'
+    });
+}]);
