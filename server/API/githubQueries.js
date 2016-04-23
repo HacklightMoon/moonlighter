ca1576eff45de620da22358b58b56d2443bc2ea2
Patrick Daly <pdaly@me.com>
2016-04-22 19:43:48 -0500

    Add functions to handle GET requests to the github API to obtain data on each issue.

--- server/API/githubQueries.js
+++ server/API/githubQueries.js
@@ -20,4 +20,21 @@ module.exports.firstTry = function(url){
   });
 }
 
+module.exports.notifications = function(){
+  return new Promise(function(resolve, reject){
+    let options = {
+      url: 'https://api.github.com/search/issues?q=is%3Aopen+is%3Aissue+mentions%3Amoonlighter-bot',
+      headers: {
+        "User-Agent": 'Moonlight'
+      }
+    };
 
+    request.get(options, function(err, resp, body){
+      if(err){
+        reject(err);
+        return;
+      }
+      resolve(body, resp);
+    });
+  })
+}
