56dceb864e87ac206ea4ed8f6a3569272b47b236
Richard Boothe <richardjboothe@me.com>
2016-04-23 12:18:08 -0500

    add editQuest

--- client/app/scripts/controllers/landing.js
+++ client/app/scripts/controllers/landing.js
@@ -1,5 +1,5 @@
 'use strict';
 angular.module('moonlighterApp')
 .controller('landingCtrl', ['$scope', function($scope) {
- 
+  $scope.name = 'Main';
 }]);
--- client/app/scripts/controllers/questFeed.js
+++ client/app/scripts/controllers/questFeed.js
@@ -1,5 +1,5 @@
 'use strict';
 angular.module('moonlighterApp')
 .controller('ShowsController', ['$scope','ShowsService', function($scope, ShowsService) {
-    $scope.shows = ShowsService.list();
+    $scope.quest = ShowsService.list();
  }]);
