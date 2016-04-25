f389a964c74e04224ff2c67ad3b537ab70de9c2b
John Jacob <jacobjohnrobert@gmail.com>
2016-04-25 12:23:07 -0500

    create GET quest endpoint, quest schema, Quest model methods

---
+++ server/models/quests.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let Promise = require('bluebird'); //remove this once babel is available.
+let db = require('../db');
+let Quests = module.exports;
+
+Quests.getById = function(id){
+  return db('quests').where({
+    'id': id
+  }).limit(1);
+}
+
+Quests.getByTitle = function(title){
+  return db('quests').where({
+    'title': title
+  });
+}
