51790f14320201d2c489d6e3bea1d329d481a04b
John Jacob <jacobjohnrobert@gmail.com>
2016-04-25 14:55:08 -0500

    added several quest search methods

--- server/models/quests.js
+++ server/models/quests.js
@@ -2,22 +2,52 @@
 
 let Promise = require('bluebird'); //remove this once babel is available.
 let db = require('../db');
+let Users = require('./users');
 let Quests = module.exports;
 
 Quests.getById = function(id){
   return db('quests').where({
     'id': id
   }).limit(1);
-}
+};
 
 Quests.getByTitle = function(title){
   return db('quests').where({
     'title': title
   });
-}
+};
 
 Quests.getByType = function(type){
   return db('quests').where({
     'type': type
   });
+};
+
+Quests.getByCreator = function(github_username){
+  return db('users').where({
+    'github_username': github_username
+  }).select('id').limit(1)
+  .then(function(id){
+    return db('quests').where({
+      'creator': id
+    });
+  });
+};
+
+Quests.getByTechnology = function(name){
+  return db('technologies').where({
+    'name': name
+  }).select('id').limit(1)
+  .then(function(id){
+    return db('questsTechnologies').where({
+      'technology_id': id
+    }).select('quest_id')
+  })
+  .then(function(ids){
+    return db('quests').whereIn('id', ids);
+  });
 }
+
+Quests.getAll = function(id){
+  return db('quests');
+};
