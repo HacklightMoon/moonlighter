f8d0d211ec6bfc8d09efed29698321b960b11e8d
Matthew J. Kelly <matjkel@icloud.com>
2016-04-25 22:09:19 -0500

    Replace creator with user_id for readability

--- server/seeds/quests_seed.js
+++ server/seeds/quests_seed.js
@@ -4,7 +4,8 @@ exports.seed = function(knex, Promise){
     knex('quests').del(),
     
     knex('quests').insert({
-    creator: 1,
+
+    user_id: 1,
     title: "Yelo",
     type: "project",
     stack: ['node.js', 'express.js','backbone.js', 'mongoDB' ],
@@ -13,7 +14,8 @@ exports.seed = function(knex, Promise){
   }),
 
     knex('quests').insert({
-    creator: 2,
+
+    user_id: 2,
     title: "KillerRx",
     type: "project",
     stack: ['ruby','postgres','ruby on rails','cowboy'],
@@ -22,7 +24,8 @@ exports.seed = function(knex, Promise){
   }),
 
     knex('quests').insert({
-    creator: 2,
+
+    user_id: 3,
     title: "PiratesMeet",
     type: "project",
     stack: ['react.js','ember.js', 'handlebars.js'],
@@ -31,7 +34,8 @@ exports.seed = function(knex, Promise){
   }),
 
     knex('quests').insert({
-    creator: 3,
+
+    user_id: 3,
     title: "Yankly",
     type: "project",
     stack: ['react.js','ember.js', 'handlebars.js'],
@@ -40,7 +44,7 @@ exports.seed = function(knex, Promise){
   }),
 
     knex('quests').insert({
-    creator: 3,
+    user_id: 3,
     title: "Butterz",
     type: "project",
     stack: ['react.js','ember.js', 'handlebars.js'],
