c639661632178fd05e0e17dc9e068077ff6cf90e
Patrick Daly <pdaly@me.com>
2016-04-26 00:57:13 -0500

    Build out newQuest controller and clean up services.

--- client/app/controllers/newQuestController.js
+++ client/app/controllers/newQuestController.js
@@ -1,5 +1,42 @@
 'use strict';
 angular.module('moonlighterApp')
 .controller('NewQuestCtrl', ['$scope', function($scope) {
-  $scope = {};
+  $scope.newQuest = {};
+
+  $scope.addQuest = function () {
+    $scope.titleRequired = '';
+    $scope.descriptionRequired = '';
+    $scope.techRequired = '';
+    $scope.urlRequired = '';
+    $scope.bountyRequired = '';
+
+    if (!$scope.newQuest.title) {
+      $scope.titleRequired = 'Title Required';
+    }
+
+    if (!$scope.newQuest.description) {
+      $scope.descriptionRequired = 'Description Required';
+    }
+
+    if (!$scope.newQuest.tech) {
+      $scope.techRequired = 'Tech Stack Required';
+    }
+
+    if (!$scope.newQuest.url) {
+      $scope.urlRequired = 'URL Required';
+    }
+
+    if (!$scope.newQuest.bounty) {
+      $scope.bountyRequired = 'Bounty Required';
+    }
+
+    var quest = {
+      "type": "project",
+      "description": $scope.newQuest.description,
+      "title": $scope.newQuest.title,
+      "tech": $scope.newQuest.tech.split(','),
+      "url": $scope.newQuest.url,
+      "bounty": $scope.newQuest.bounty
+    }
+  }
 }]);
