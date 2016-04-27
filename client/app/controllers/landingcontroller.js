22d677d447c4292b795584260cf827c031ca85e5
Richard Boothe <richardjboothe@me.com>
2016-04-27 12:08:17 -0500

    add style

--- client/app/controllers/landingcontroller.js
+++ client/app/controllers/landingcontroller.js
@@ -1,5 +1,13 @@
 'use strict';
 angular.module('moonlighterApp.landing',[])
 .controller('LandingCtrl', ['$scope', function($scope) {
-  $scope.boolValue = true;
-}]);
+  
+}])
+
+// ng-model="checked" ng-init="checked=true"
+
+// <span ng-if="checked" class="animate-if">
+//   This is removed when the checkbox is unchecked.
+// </span>
+
+// $state.current.name
