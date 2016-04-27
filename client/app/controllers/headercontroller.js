ce9e95ba42f984386152fe2e2e0b89fd0054db99
Richard Boothe <richardjboothe@me.com>
2016-04-26 19:17:15 -0500

    add link behavior to home page

--- client/app/controllers/headercontroller.js
+++ client/app/controllers/headercontroller.js
@@ -1,8 +1,9 @@
 'use strict';
 angular.module('moonlighterApp.header',[])
 .controller('HeaderCtrl',['$state', '$scope', function($state, $scope){
+    $scope.seen = true;
     if($state.current.name==="home"){
-
+      $scope.seen = false;
     };
     console.log($state);
 }]);
