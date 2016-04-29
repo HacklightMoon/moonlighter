18d11d7b8d135a574af7925f7ee4162b482263b7
John Jacob <jacobjohnrobert@gmail.com>
2016-04-29 15:01:35 -0500

    :(

--- server/auth/config.js
+++ server/auth/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   'github': {
     'clientID': 'ddf9ccf373421e6aa62d',
-    'clientSecret': '4b810786513bf1910bba5293b10f4b30cc2d46e8'
+    'clientSecret': '4b810786513bf1910bba5293b10f4b30cc2d46e8',
     'callbackURL': '/auth/github/callback'
   }
 }
