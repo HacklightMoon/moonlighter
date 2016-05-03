e43638685f3f223a51c629f5c0d891c13cd33dc9
John Jacob <jacobjohnrobert@gmail.com>
2016-05-02 19:28:52 -0500

    hide classified information.

--- server/auth/config.js
+++ server/auth/config.js
@@ -2,15 +2,15 @@
 let config = require('../config/environment');
 
 let development = {
-  'clientID': 'ddf9ccf373421e6aa62d',
-  'clientSecret': '4b810786513bf1910bba5293b10f4b30cc2d46e8',
-  'callbackURL': 'http://127.0.0.1:3000/auth/github/callback'
+  'clientID': process.env.GITHUB_CLIENT_ID_DEVELOPMENT,
+  'clientSecret': process.env.GITHUB_CLIENT_SECRET_DEVELOPMENT,
+  'callbackURL': process.env.CALLBACK_URL_DEVELOPMENT
 };
 
 let production = {
-  'clientID': '17fad02a6b4e1d53a0d6',
-  'clientSecret': '802b53f6e98025d9137b701b076832c657920c68',
-  'calbackURL': 'https://powerful-ocean-19597.herokuapp.com/auth/github/callback'
+  'clientID': process.env.GITHUB_CLIENT_ID_PRODUCTION,
+  'clientSecret': process.env.GITHUB_CLIENT_SECRET_PRODUCTION,
+  'callbackURL': process.env.CALLBACK_URL_PRODUCTION
 };
 
 module.exports = config.environment === "production" ? production : development;
