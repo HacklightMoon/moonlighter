b04522004abd3aa554c678d63125bf3d1436d3ab
John Jacob <jacobjohnrobert@gmail.com>
2016-05-06 19:42:01 -0500

    fix db file for heroku

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 'use strict';
 
 let config = require('./knexfile');
-let env = process.env.NODE_env || 'development';
+let env = process.env.NODE_ENV || 'development';
 let knex = require('knex')(config[env]);
 module.exports = knex;
