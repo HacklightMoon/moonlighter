966a8e032ef36eef1475766d24ed07605c554012
Ion Gorincioi <gorincioii@yahoo.com>
2016-05-10 20:07:39 -0500

    esrver Api

--- server/server.js
+++ server/server.js
@@ -220,7 +220,7 @@ app.post('/codewars/api', function(req, res){
   console.log("req.body", req.body)
   Users.linkCodewars(req.body.userID, req.body.cwUsername, req.body.cwUserAPI)
   .then(function(resp){
-    res.send(resp[0]);
+    res.send(resp);
   })
   .catch(function(err){
     console.log("error:", err);
