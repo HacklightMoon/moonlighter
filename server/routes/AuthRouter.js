56ec8117042940f6fbe658e1862f2b5ce9c9eb0b
Richard Boothe <richardjboothe@users.noreply.github.com>
2016-08-30 12:56:44 -0400

    Routerbranch (#313)
    * modify package.json
    
    * divide routes into respective features for organization
    
    * fix syntax error
    
    * further clean up of server and routing.
    
    * create characterrouter, and remove unneccessary variables

---
+++ server/routes/AuthRouter.js
@@ -0,0 +1,28 @@
+let express        = require('express');
+let Path           = require('path');
+let AuthRouter     = express.Router();
+let passportGithub = require('./../auth/github');
+
+
+AuthRouter.get('/github/callback', passportGithub.authenticate('github', {
+  failureRedirect: '/auth/github',
+  successRedirect: '/'
+}));
+
+// Logout route
+AuthRouter.get('/logout', function(req,res){
+  req.session.destroy(function(){
+    res.clearCookie('connect.sid');
+    req.signedCookies = null;
+    req.logout();
+    res.redirect('/');
+  });
+});
+
+//Authentication Route
+AuthRouter.get('/github', passportGithub.authenticate('github', {
+  scope: ['user', 'public_repo', 'notifications']
+}));
+
+module.exports = AuthRouter;
+
---
+++ server/routes/CharacterRouter.js
@@ -0,0 +1,20 @@
+let express          = require('express');
+let CharacterRouter  = express.Router();
+let Path             = require('path');
+let passportGithub   = require('./../auth/github');
+let Character        = require('./../models/characters');
+
+//--------------------Character Endpoints----------------
+
+CharacterRouter.get('/', function(req, res){
+  Character.getByLevel(req.query.level)
+  .then(function(characters){
+    res.send(characters[0]);
+  })
+  .catch(function(err){
+    console.error(err);
+  });
+});
+
+module.exports = CharacterRouter;
+
